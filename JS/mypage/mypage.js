b8c837f4f7082e5d7ae0fd4d507957865100e545
justcodig <https://github.com/justcodig>
2024-05-20 21:41:27 +0900

    1완료 수정할수도 있음

--- JS/mypage/mypage.js
+++ JS/mypage/mypage.js
@@ -1,46 +1,32 @@
 ///// 이미지업로드화면 none으로 준거 프로필 변경하기 누르면 보이게하기 //////
 document.querySelector('.change_profile').addEventListener('click', function(){
-    document.querySelector('#test').classList.remove('myimage')
+    document.querySelector('#test').classList.remove('myimage');
+    document.querySelector('#image_head').style.zIndex ='2'
 });
 
 
 //////// 이미지 업로드 화면 띄어져있는상태에서 주변빈곳누르면 사라지게 만들기 /////// 
 top.onclick = (e) => {
-    console.log(e.target)
+    // console.log(e.target)
     if(e.target == nickname){
     document.querySelector('#test').classList.add('myimage');
-    }e
+    }
 }
 
 
 /////////// 객체를 묶는  이미지에 값을 null이 아닌 이미지 값으로 넣기 //////////
-localStorage.setItem('User', JSON.stringify([{id : "login", nick : "1234", image : null }]))
+ localStorage.setItem('User', JSON.stringify([{id : "이경재", password : "1234", nick : "bb", image : null }]))
 /////////////////////////////////////////////////////////////////
-
+sessionStorage.setItem('login', "이경재" );
 
 
 
 /////// 닉네임변경 글씨입력하는곳에 변경하기 하면 로컬에 넣고 재부팅하면 변경한값들어가있기 ////
 document.querySelector(".change_button").addEventListener("click", function(){
-    function saveData(){
-        localStorage.setItem("User", document.querySelector('#loc').value);
-    }
-    saveData();    
-    
-    function getData(){
-        localStorage.getItem("User");
-        console.log(localStorage.getItem("User"));
-        
-    }
-    getData();
-
-
-    document.querySelector('#loc').value = localStorage.getItem("User");
-
 
     const uid = sessionStorage.getItem("login");
     const deee = JSON.parse(localStorage.getItem("User"));
-    
+
     for(let i =0; i< deee.length; i++){
         if(deee[i].id === uid){
             deee[i].nick = document.querySelector("#loc").value;
@@ -48,30 +34,18 @@ document.querySelector(".change_button").addEventListener("click", function(){
         }
     }
 });
-document.querySelector('#loc').value = localStorage.getItem("User");
-
+// document.querySelector('#loc').value = localStorage.getItem("User");
 
 
 ////// 닉네임 객체배열안에 꺼내오기 /////
-document.querySelector('#loc').value ;
-const a=JSON.parse(localStorage.getItem("User"));
-
-for(let i=0; i<a.length; i++){
-    document.querySelector('#loc').value = a[i].nick;
-    
+const nick_change = JSON.parse(localStorage.getItem("User"));
+const login_id = sessionStorage.getItem("login");
+// 만약에 세션스토리지에있는 유저안에 아이디가 로컬스토리지 배열안에 있는 아이디와 같다면 그안에있는 닉네임만 #loc안에 불러와줘
+for(let i = 0; i<nick_change.length; i++){
+    if(nick_change[i].id === login_id){
+        document.querySelector("#loc").value = nick_change[i].nick;
 }
-
-
- 
-
-
-
-
-
-
-
-
-
+}
 
 
 
@@ -99,7 +73,7 @@ document.querySelector("#chooseFile").addEventListener("change", function(e){
     }
     getData();
     
-    // console.log(document.querySelector('.background').imgBase64)
+    console.log(document.querySelector('.background').imgBase64)
     document.querySelector('.background').imgBase64 = localStorage.getItem(".background");
 });
 document.querySelector('.background').imgBase64 = localStorage.getItem(".background");
@@ -129,8 +103,6 @@ chooseFile.addEventListener("change", (e) => {
         newImg.src = imgBase64;
 
 
-
-
         newImg.style.width = "170px";
         newImg.style.height = "170px";
         newImg.draggable = false;
@@ -138,7 +110,7 @@ chooseFile.addEventListener("change", (e) => {
        
         document.querySelector('.button2').append(newImg);
         ////// 사진 이미지 로컬스토리지 배열안에 저장해줌 //////// 
-        localStorage.setItem('User', JSON.stringify([{id : "login", nick : "1234", "image" : imgBase64 }]))
+        localStorage.setItem('User', JSON.stringify([{id : "이경재", password : "1234" , nick : "bb", "image" : imgBase64 }]))
     }
 
     reader.readAsDataURL(img);
@@ -147,6 +119,19 @@ chooseFile.addEventListener("change", (e) => {
 document.querySelector("#chooseFile").addEventListener("change", function(){
     document.querySelector('#test').classList.add('myimage');
 })
+
+
+
+
+////////////////// x표시 누르면 다른 페이지로 이동 //////////////////////////
+document.querySelector(".x_imge").addEventListener("click",function(){
+   location.href = "../../HTML/board/board.html"
+})
+
+
+
+
+///////////////////////  유저 정보가 담긴 객체를 여러개 만들어서 여러개 있는 객체 정보중에 한개 찝어서 거기 있는 nick 바꾸기 ////////////////
 
 
 
@@ -179,9 +164,6 @@ document.querySelector("#chooseFile").addEventListener("change", function(){
 // 업로드페이지가 뜬걸 빈곳아무곳이나 눌러서 없애기
 // 이미지 로컬로 받아온거 미리보기에 들어올수있게 하기
 // 
-
-
-
 
 
 
