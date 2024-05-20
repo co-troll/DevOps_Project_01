42e937145433b55f37ed03adf1ea6b7830b49b65
joonghyeondevelop <https://github.com/joonghyeondevelop>
2024-05-20 11:22:34 +0900

    [feat] : 로그아웃 기능 구현, 마우스 커서 변경, 메인, 게시판 나눔

--- JS/help/Board.js
+++ JS/help/Board.js
@@ -10,37 +10,110 @@ function render (){
     let listCreate = document.querySelector('.board_list')
     let show = JSON.parse(localStorage.getItem('Board'));
 
-    console.log(show)
-    for(let i = 0; i < show.length; i++){
-        let list_ul = document.createElement('ul');
-        // let list_category = document.createElement('li');
-        let list_no = document.createElement('li');
-        let list_title = document.createElement('li');
-        let list_writer = document.createElement('li');
-        let list_view = document.createElement('li');
-        let list_date = document.createElement('li');
-
-        listCreate.append(list_ul);
-        list_ul.append(list_no, list_title, list_writer, list_date, list_view);
-
-        list_no.innerHTML = i + 1;
-        list_title.innerHTML = show[i].title;
-        list_writer.innerHTML = '안중현'; // 나중에 바꿔야 함
-        // list_date.innerHTML = new date()
-        list_view.innerHTML = i + 1; // 나중에 바꿔야 함
+    if(show !== null){
+        for(let i = 0; i < show.length; i++){
+            let list_ul = document.createElement('ul');
+            let list_no = document.createElement('li');
+            let list_title = document.createElement('li');
+            let list_writer = document.createElement('li');
+            let list_view = document.createElement('li');
+            let list_date = document.createElement('li');
+    
+            listCreate.append(list_ul);
+            list_ul.append(list_no, list_title, list_writer, list_date, list_view);
+    
+            list_no.innerHTML = i + 1;
+            list_title.innerHTML = show[i].title; // 제목 출력
+            list_date.innerHTML = show[i].date; // 날짜 출력
+            list_writer.innerHTML = show[i].nick; // 작성자 출력
+            list_view.innerHTML = show[i].view;
+        }
     }
 
     let loginID = sessionStorage.getItem('login');
     let localArray = JSON.parse(localStorage.getItem('User'));
     let showID = document.querySelector('.login_id');
-
-    for(let i=0; i < localArray.length; i++){
-        if(loginID === localArray[i].id){
-            showID.innerHTML = localArray[i].nick
+    if(localArray !== null){ // 헤더 닉네임 출력 칸
+        for(let i=0; i < localArray.length; i++){
+            if(loginID === localArray[i].id){
+                showID.innerHTML = localArray[i].nick
+            }
         }
     }
 }
 
 render();
 
-let login_btn = document.querySelector('.loginBtn');
+let createWrite = document.querySelector('.createList');
+
+createWrite.onclick = () => {
+    if(sessionStorage.getItem('login') === null){
+        alert('로그인이 되지 않아 게시글을 작성 할 수 없습니다.');
+        return;
+    }
+    location.href = "write.html"
+}
+
+let helpBoard = document.querySelector('.help_board');
+let helpBoard2 = document.querySelector('.help_board2');
+let firstTab = document.querySelector('.category_test');
+let secondTab = document.querySelector('.no_test');
+let hide = document.querySelector('.hide');
+let mainShow = document.querySelector('.main_show')
+
+
+firstTab.onclick = () => { // 첫번째 탭을 눌렀을 시 발생하는 이벤트
+    mainShow.classList.add('hide');
+    firstTab.classList.add('checking');
+    secondTab.classList.remove('checking');
+    helpBoard.classList.add('animate');
+    helpBoard.classList.remove('hide');
+    helpBoard.style.display = '';
+    helpBoard.style.zIndex = '4';
+    helpBoard2.style.zIndex = '3';
+    helpBoard2.style.display = 'none'
+    setTimeout(() => { // 0.5초 뒤에 helpBoard에 no1color Class 추가
+        helpBoard.classList.add('no1color')
+    }, 500)
+    setTimeout(() => { // 1초 뒤에 helpBoard에 animate Class 삭제
+        // let helpBoard = document.querySelector('.help_board');
+        helpBoard.classList.remove('animate')
+    }, 1000);
+}
+
+secondTab.onclick = () => { // 두번쨰 탭을 눌렀을 시 발생하는 이벤트
+    mainShow.classList.add('hide');
+    secondTab.classList.add('checking');
+    firstTab.classList.remove('checking');
+    helpBoard.classList.add('hide');
+    helpBoard2.classList.add('animate');
+    helpBoard2.classList.remove('hide');
+    helpBoard.style.display = 'none'
+    helpBoard.style.zIndex = '3';
+    helpBoard2.style.zIndex = '4';
+    helpBoard2.style.display = '';
+    setTimeout(() => { // 0.5초 뒤에 helpBoard2에 no2color Class 추가
+        helpBoard2.classList.add('no2color')
+    }, 500)
+    setTimeout(() => { // 1초 뒤에 helpBoard2에 animate Class 삭제
+        // let helpBoard = document.querySelector('.help_board');
+        helpBoard2.classList.remove('animate')
+    }, 1000);
+}
+
+
+let logout_button = document.querySelector('.logout_button');
+
+logout_button.onclick = () => {
+    let headID = document.querySelector('.login_id');
+    if(sessionStorage.getItem('login') === null){
+        alert('로그인이 되지 않았습니다.')
+    }else if(sessionStorage.getItem('login') !== null){
+        headID.innerHTML = "";
+        sessionStorage.clear();
+        alert('로그아웃 됬습니다.');
+        location.href = '../../HTML/login/login.html'
+    }
+}
+
+// 클릭을 했을때, 화면이 지워지면서 밑으로 접히는 애니메이션 넣기
--- JS/help/write.js
+++ JS/help/write.js
@@ -7,10 +7,13 @@ let categoryList = document.querySelector('.category_list');
 let pageLoad = JSON.parse(localStorage.getItem('Board')) || []; // 페이지 로드시 board local 빈값이면 빈배열 반환
 
 class Board {
-    constructor(category, title, content){
+    constructor(category, title, content, nick, date, view){
         this.category = category;
         this.title = title;
         this.content = content;
+        this.nick = nick;
+        this.date = date;
+        this.view = view;
     }
 }
 
@@ -18,8 +21,38 @@ createContent.onclick = function move() { // 작성 버튼 누를 시 작성 값
     let content = document.querySelector('.text_content');
     let title = document.querySelector('.text_title');
     let categoryList = document.querySelector('.category_list');
-    
-    let boardObject = new Board(categoryList.value, title.value, content.value);
+    let view = 0;
+
+    function getDate() { // 로컬에 날짜 출력하려고 만든 함수
+        const today = new Date();
+      
+        const year = today.getFullYear(); // 년도
+        const month = (today.getMonth() + 1).toString().padStart(2, '0'); // 월
+        const day = today.getDate().toString().padStart(2, '0'); // 일
+      
+        const dateString = year + '-' + month + '-' + day;  // 년 - 월 - 일
+      
+        return dateString;
+      }
+
+
+    //////// 글 작성시 로컬에 작성자 닉네임 저장하려고 작성함
+
+    let loginID = sessionStorage.getItem('login'); // 세션 값 가져오기
+    let localArray = JSON.parse(localStorage.getItem('User')); // 로컬 값 가져오기.
+    let saveID;
+
+    if(localArray !== null){ // 로컬이 null이 아니라면 for문을 돌림
+        for(let i=0; i < localArray.length; i++){
+            if(loginID === localArray[i].id){ // loginID와 local[i].id가 같다면
+                saveID = localArray[i].nick // id와 일치하는 객체의 닉네임을 saveID에 할당.
+            }
+        }
+    }
+
+    /////////////
+
+    let boardObject = new Board(categoryList.value, title.value, content.value, saveID, getDate(), view); // 작성자도 넣어야 됨.
     
     if(title.value === "" || content.value === ""){ // content, title 빈값이면 작성 못하게 막음
         return;
@@ -29,21 +62,3 @@ createContent.onclick = function move() { // 작성 버튼 누를 시 작성 값
     
     location.href = 'Board.html'
 }
-
-// createContent.addEventListener('click', function(){
-//     let content = document.querySelector('.text_content');
-//     let title = document.querySelector('.text_title');
-//     let categoryList = document.querySelector('.category_list');
-
-//     let boardObject = new Board(categoryList.value, title.value, content.value);
-
-//     if((title.value && content.value) === ""){ // content, title 빈값이면 작성 못하게 막음
-//         return;
-//     }
-//     pageLoad.push(boardObject)
-//     localStorage.setItem('Board', JSON.stringify(pageLoad));
-
-//     render();
-
-//     location.href = 'Board.html'
-// })
