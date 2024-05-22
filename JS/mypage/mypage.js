08b5b24740779ad99a3d86556ee822da3f99f84e
justcodig <https://github.com/justcodig>
2024-05-22 09:24:47 +0900

    mypage 수정

--- JS/mypage/mypage.js
+++ JS/mypage/mypage.js
@@ -5,19 +5,21 @@ document.querySelector('.change_profile').addEventListener('click', function(){
 });
 
 
+
 //////// 이미지 업로드 화면 띄어져있는상태에서 주변빈곳누르면 사라지게 만들기 /////// 
 top.onclick = (e) => {
-    // console.log(e.target)
     if(e.target == nickname){
     document.querySelector('#test').classList.add('myimage');
     }
 }
 
 
+
 /////////// 객체를 묶는  이미지에 값을 null이 아닌 이미지 값으로 넣기 //////////
  localStorage.setItem('User', JSON.stringify([{id : "이경재", password : "1234", nick : "bb", image : null }]))
 /////////////////////////////////////////////////////////////////
 sessionStorage.setItem('login', "이경재" );
+
 
 
 
@@ -35,6 +37,7 @@ document.querySelector(".change_button").addEventListener("click", function(){
     }
 });
 // document.querySelector('#loc').value = localStorage.getItem("User");
+
 
 
 ////// 닉네임 객체배열안에 꺼내오기 /////
@@ -68,7 +71,6 @@ document.querySelector("#chooseFile").addEventListener("change", function(e){
 
     function getData(){
         localStorage.getItem("User");
-        // console.log(localStorage.getItem("User"));
     
     }
     getData();
@@ -131,39 +133,11 @@ document.querySelector(".x_imge").addEventListener("click",function(){
 
 
 
-///////////////////////  유저 정보가 담긴 객체를 여러개 만들어서 여러개 있는 객체 정보중에 한개 찝어서 거기 있는 nick 바꾸기 ////////////////
-
-
-
-
-/////// 로컬스토리지에 저장하기 
-// document.querySelector('.change_button').addEventListener("click", () => {
-// const uid = sessionStorage.getItem("login");
-// const deee = JSON.parse(localStorage.getItem("user"));
-
-// for(let i =0; i< deee.length; i++){
-//     if(deee[i].id === uid){
-//         deee[i].nick = document.querySelector("#loc").value;
-//         localStorage.setItem("user", JSON.stringify(deee))
-//     }
-// }
-// });
 
 
 
 
 
-
-// 여기서부터해야 할것은 id/password/nick/image 배열안에객체로만들어서 로컬에 저장하기
-// 이미지를 넣음 기존이미지 삭제 추가 이미지보여줌 
-// 프로필변경 페이지로 들어옴 기존닉네임과 함께 같이 로컬로 저장된걸 받아서 가져옴
-// 닉네임 변경하기를 누르면 닉네임이 변경됨
-// 프로필 변경하기를 누르면 이미지 업로드페이지가 뜸
-// 업로드페이지에서 이미지 사진을 바꾸면 미리보기에 사진이 들어감
-// 사진이 맘에안들면 삭제도됨 
-// 업로드페이지가 뜬걸 빈곳아무곳이나 눌러서 없애기
-// 이미지 로컬로 받아온거 미리보기에 들어올수있게 하기
-// 
 
 
 
